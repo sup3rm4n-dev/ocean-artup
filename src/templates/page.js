60b743e42c62cb70538729d209fa489c39ca3b2a
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-07-16 21:53:27 +0200

    feat: remove subtitle from page template

--- src/templates/page.js
+++ src/templates/page.js
@@ -8,13 +8,13 @@ import PageBody from '../components/PageBody'
 
 const PageTemplate = ({ data, location }) => {
   const { page, site } = data
-  const { title, subtitle, body } = page
+  const { title, body } = page
   const { excerpt, html } = body && body.data
   const path = location.pathname
   return (
     <Layout>
       <Helmet pageTitle={title} site={site} path={path} description={excerpt} />
-      <PageTitle title={title} subtitle={subtitle} />
+      <PageTitle title={title} />
       {html && <PageBody dangerouslySetInnerHTML={{__html: html}} />}
     </Layout>
   )
@@ -35,7 +35,6 @@ export const pageQuery = graphql`
     ...siteMetaQuery
     page: contentfulPage(slug: {eq: $slug}) {
       title
-      subtitle
       slug
       body {
         data: childMarkdownRemark {
