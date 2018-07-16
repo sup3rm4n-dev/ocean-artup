35d65e85f2ab630015bfd96d1b4f48e83cc01f74
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-07-16 12:08:29 +0200

    feat: landing page

---
+++ src/pages/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import Layout from '../components/Layout' 
+import Helmet from '../components/Helmet'
+import PageTitle from '../components/PageTitle'
+import LandingPageBody from '../components/LandingPageBody'
+
+const LandingPage = ({ data, location }) => {
+  const { page, site } = data
+  const { title, subtitle, body } = page
+  const { excerpt, html } = body && body.data
+  const path = location.pathname
+  return (
+    <Layout>
+      <Helmet pageTitle={title} site={site} path={path} description={excerpt} />
+      <PageTitle title={title} subtitle={subtitle} />
+      {html && <LandingPageBody dangerouslySetInnerHTML={{__html: html}} />}
+    </Layout>
+  )
+}
+
+export default LandingPage
+
+export const landingPageQuery = graphql`
+  query Landing {
+    ...siteMetaQuery
+    page: contentfulLandingPage(slug: {eq: "landing"}) {
+      title
+      subtitle
+      body {
+        data: childMarkdownRemark {
+          excerpt
+          html
+        }
+      }
+    }
+  }
+`
