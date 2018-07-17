24b458ca956097d917558f665f1c31be60c1c72b
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-07-17 15:33:49 +0200

    feat: add BackLink to PostTitle

--- src/components/PostTitle/index.js
+++ src/components/PostTitle/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Container, Title, Meta } from './styles'
+
+import { Container, Title, Meta, BackLink } from './styles'
 import { DateIcon, TimeIcon } from '../Icons'
 
 const PostTitle = props => (
   <Container>
     <Title>{props.title}</Title>
     <Meta>
-      <DateIcon /> {props.date} | <TimeIcon /> {props.timeToRead} Min Lesezeit
+      <DateIcon />&ensp;{props.date}&ensp;|&ensp;<TimeIcon /> {props.timeToRead} min read
     </Meta>
+    <BackLink to='/blog'>Back to blog</BackLink>
   </Container>
 )
 
