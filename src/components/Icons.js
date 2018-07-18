3fe95f05db09f0469945ae1d60b677baf98f18b7
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-07-18 13:09:49 +0200

    refactor: Icons

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,35 +1,25 @@
-import styled from 'styled-components'
-import { Calendar } from 'styled-icons/octicons/Calendar.cjs'
-import { Timer } from 'styled-icons/material/Timer.cjs'
-import { Email } from 'styled-icons/material/Email.cjs'
-import { FacebookSquare } from 'styled-icons/fa-brands/FacebookSquare.cjs'
-import { Github } from 'styled-icons/fa-brands/Github.cjs'
-import { Linkedin } from 'styled-icons/fa-brands/Linkedin.cjs'
+import styled, { css } from 'styled-components'
+import { Calendar } from 'styled-icons/octicons/Calendar'
+import { Timer } from 'styled-icons/material/Timer'
+import { Email } from 'styled-icons/material/Email'
+import { Create } from 'styled-icons/material/Create'
+
+const defaultHeight = css`
+  height: ${props => props.size || '1rem'};
+`
 
 export const DateIcon = styled(Calendar)`
-  height: ${props => props.size || '1rem'};
-  position: relative;
-  top: -3px;
+  ${defaultHeight}
 `
 
 export const TimeIcon = styled(Timer)`
-  height: ${props => props.size || '1rem'};
-  position: relative;
-  top: -2px;
+  ${defaultHeight}
 `
 
 export const EmailIcon = styled(Email)`
-  height: ${props => props.size || '1rem'};
+  ${defaultHeight}
 `
 
-export const FacebookIcon = styled(FacebookSquare)`
-  height: ${props => props.size || '1rem'};
-`
-
-export const GitHubIcon = styled(Github)`
-  height: ${props => props.size || '1rem'};
-`
-
-export const LinkedinIcon = styled(Linkedin)`
-  height: ${props => props.size || '1rem'};
+export const CreateIcon = styled(Create)`
+  ${defaultHeight}
 `
