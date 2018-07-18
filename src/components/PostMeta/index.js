c2875d39473de5554b77b11d627f9f8ae0e44e48
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-07-18 14:05:28 +0200

    feat PostMeta comp

---
+++ src/components/PostMeta/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { Meta, Category, AuthorPhoto } from './styles'
+import { DateIcon, TimeIcon, CreateIcon } from '../Icons'
+
+const PostMeta = ({ author, date, categories, body, inTitle }) => (
+  <Meta inTitle={inTitle} >
+    <span>
+      <CreateIcon />
+      &nbsp;
+      <a href={`mailto:${author.email}`}>{author.name}</a>
+      <AuthorPhoto src={author.photo.file.url} />
+    </span>
+    &ensp;|&ensp;
+    <span>
+      <DateIcon />
+      &ensp;{date}
+    </span>
+    &ensp;|&ensp;
+    <span>
+      <TimeIcon />
+      &ensp;{body.data.timeToRead} min read
+    </span>
+    &ensp;|&ensp;
+    <span>{categories.map(category =>
+      <Category key={category.slug}>{category.title}</Category>
+    )}</span>
+  </Meta>
+)
+
+export default PostMeta
---
+++ src/components/PostMeta/styles.js
@@ -0,0 +1,46 @@
+import styled, { css } from 'styled-components'
+
+import mediaQuery from '../../utils/mediaQuery'
+
+export const Meta = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  * {
+    margin: 0;
+  }
+  > span {
+    display: flex;
+    align-items: center;
+  }
+  align-items: center;
+  margin: 1.3rem 0 1rem;
+  font-size: 0.9rem;
+  ${props => props.inTitle && css`
+    justify-content: center;
+    max-width: 80%;
+    width: max-content;
+    padding: 0.2rem 0.6rem;
+    margin: 0.75rem auto 1.5rem;
+    color: ${props => props.theme.mainOrange};
+    border: ${({theme}) => theme.smallBorder + ' solid ' + theme.mainWhite};
+    background: ${props => props.theme.mainGray};
+    border-radius: ${props => props.theme.largeBorderRadius};
+    ${Category} {
+      color: ${props => props.theme.mainGray};
+      background: ${props => props.theme.mainOrange};
+    }
+  `}
+`
+
+export const Category = styled.span`
+  padding: 0 0.5rem;
+  border-radius: ${props => props.theme.largeBorderRadius};
+  background: ${props => props.theme.lightGray};
+  margin-left: 0.5rem;
+`
+
+export const AuthorPhoto = styled.img`
+  width: 2rem;
+  border-radius: 50%;
+  margin: 0 0 0 0.5rem;
+`
