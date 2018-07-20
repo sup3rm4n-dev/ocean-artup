014c5b18a04e8daeda98ae473212d2a97fb8fc6e
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-07-20 23:20:51 +0200

    chore: yarn remove gatsby-remark-images-contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,16 +17,6 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          {
-            resolve: `gatsby-remark-images-contentful`,
-            options: {
-              // maxWidth (in pixels) of content container
-              // used as base for generating different widths of each image
-              maxWidth: 880,
-              showCaptions: true,
-              backgroundColor: `none`,
-            },
-          },
           `gatsby-remark-smartypants`,
           `gatsby-remark-embed-video`,
           `gatsby-remark-responsive-iframe`,
