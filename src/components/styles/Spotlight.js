882e59517c1ab16b31c67a37015b64edeea5d5a2
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-07-20 23:28:46 +0200

    feat: Spotlight comp for use in LandingPageBody

---
+++ src/components/styles/Spotlight.js
@@ -0,0 +1,54 @@
+import { css } from 'styled-components'
+
+import mediaQuery from '../../utils/mediaQuery'
+
+const Spotlight = css`
+  spotlight {
+    display: block;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+    a {
+      color: ${props => props.theme.mainOrange};
+    }
+    > p {
+      margin: 0;
+    }
+    background: ${props => props.theme.darkGray};
+    .text {
+      margin: auto;
+      padding: 3rem;
+      p {
+        max-width: ${props => props.theme.maxWidth};
+        margin-top: 1.5rem;
+      }
+      ${mediaQuery.phone} {
+        padding: 2rem 1rem;
+      }
+    }
+    ${mediaQuery.minTablet} {
+      display: flex;
+      text-align: left;
+      max-height: 25rem;
+      .text {
+        margin-left: 0;
+        max-width: ${props => props.theme.medWidth};
+      }
+      img {
+        max-width: 40vw;
+      }
+      &:nth-child(2n+1) {
+        text-align: right;
+        flex-direction: row-reverse;
+        background: ${props => props.theme.mainGray};
+        .text {
+          margin-right: 0;
+        }
+      }
+    }
+  }
+`
+
+export default Spotlight
