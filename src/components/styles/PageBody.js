6f45dd7cb65eadfc495230750310b81c2765a5a6
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-07-21 13:30:04 +0200

    style: PageBody child margins, link (hover) color and img width

--- src/components/styles/PageBody.js
+++ src/components/styles/PageBody.js
@@ -2,12 +2,27 @@ import styled from 'styled-components'
 
 import Team from './Team'
 import ImageRow from './ImageRow'
+import mediaQuery from '../../utils/mediaQuery'
 
 export const PageBody = styled.article`
   background: ${props => props.theme.mainWhite};
   padding: 10vw;
+  ${mediaQuery.minPhablet} {
+    text-align: justify;
+  }
+  * {
+    max-width: ${props => props.theme.maxWidth};
+    margin-left: auto;
+    margin-right: auto;
+  }
   a {
     color: ${props => props.theme.mainBlue};
+    :hover {
+      color: ${props => props.theme.mainOrange};
+    }
+  }
+  img {
+    width: 100%;
   }
   .caption {
     display: block;
