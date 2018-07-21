49123b48ce36ef097016048f0eddb8f6b20adbfd
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-07-21 13:28:51 +0200

    style: PostTitle link color and padding

--- src/components/PostTitle/styles.js
+++ src/components/PostTitle/styles.js
@@ -5,6 +5,9 @@ export const Container = styled.header`
   color: ${props => props.theme.mainWhite};
   text-align: center;
   margin: 10vh 0;
+  a {
+    color: inherit;
+  }
 `
 
 export const Title = styled.h1`
@@ -13,7 +16,7 @@ font-size: 1.6rem;
 
 export const BackLink = styled(Link)`
   width: max-content;
-  padding: 0 0.6rem;
+  padding: 0.2rem 0.6rem;
   border-radius: ${props => props.theme.largeBorderRadius};
   background: rgba(0, 0, 0, 0.5);
 `
