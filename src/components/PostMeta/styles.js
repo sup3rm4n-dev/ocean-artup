7e36ae210eca6ea42088ef84b6345b29032dc9a2
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-07-22 12:37:24 +0200

    style: improve responsiveness of PostMeta

--- src/components/PostMeta/styles.js
+++ src/components/PostMeta/styles.js
@@ -1,42 +1,60 @@
 import styled, { css } from 'styled-components'
 
+import mediaQuery from '../../utils/mediaQuery'
+
 export const Meta = styled.div`
-  display: flex;
-  flex-wrap: wrap;
+  display: grid;
+  grid-auto-columns: max-content;
+  grid-gap: 0.3rem;
   > span {
-    margin: 0;
     display: flex;
     align-items: center;
   }
-  align-items: center;
-  margin: 1.3rem 0 1rem;
+  margin: 1.3rem 0;
   font-size: 0.9rem;
+  ${mediaQuery.minTablet} {
+    grid-auto-flow: column;
+    grid-gap: 2rem;
+  }
   ${props => props.inTitle && css`
     justify-content: center;
+    align-items: center;
+    justify-items: center;
     max-width: 80%;
     width: max-content;
     padding: 0.2rem 0.6rem;
-    margin: 0.75rem auto 1.5rem;
+    margin: 1.5rem;
     color: ${props => props.theme.mainOrange};
     border: ${({theme}) => theme.smallBorder + ' solid ' + theme.mainWhite};
     background: ${props => props.theme.mainGray};
     border-radius: ${props => props.theme.largeBorderRadius};
-    ${Category} {
-      color: ${props => props.theme.mainGray};
-      background: ${props => props.theme.mainOrange};
+    ${mediaQuery.minTablet} {
+      grid-gap: 0.7rem;
+      >:not(:first-child) {
+        border-left: ${props => props.theme.smallBorder} solid;
+        padding-left: 0.7rem;
+      }
     }
   `}
-`
-
-export const Category = styled.span`
-  padding: 0 0.5rem;
-  border-radius: ${props => props.theme.largeBorderRadius};
-  background: ${props => props.theme.lightGray};
-  margin-left: 0.5rem;
 `
 
 export const AuthorPhoto = styled.img`
   width: 2rem !important;
   border-radius: 50%;
   margin: 0 0 0 0.5rem;
+`
+
+export const Categories = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-auto-flow: column;
+  grid-auto-columns: max-content;
+  grid-gap: 1rem;
+`
+
+export const Category = styled.span`
+  padding: 0.2rem 0.5rem;
+  border-radius: ${props => props.theme.largeBorderRadius};
+  color: ${props => props.theme.darkGray};
+  background: ${props => props.theme.mainOrange};
 `
