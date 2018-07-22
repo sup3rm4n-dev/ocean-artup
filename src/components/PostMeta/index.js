07856e97928f097d04ee8388ddfe79cae84e9bd1
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-07-22 12:37:07 +0200

    refactor: PostMeta

--- src/components/PostMeta/index.js
+++ src/components/PostMeta/index.js
@@ -1,31 +1,30 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
-import { Meta, Category, AuthorPhoto } from './styles'
+import { Meta, Categories, Category, AuthorPhoto } from './styles'
 import { DateIcon, TimeIcon, CreateIcon } from '../styles/Icons'
 
 const PostMeta = ({ author, date, categories, body, inTitle }) => (
-  <Meta inTitle={inTitle} >
-    <span>
-      <CreateIcon />
-      &nbsp;
-      <a href={`mailto:${author.email}`}>{author.name}</a>
-      <AuthorPhoto src={author.photo.file.url} />
-    </span>
-    &ensp;|&ensp;
-    <span>
-      <DateIcon />
-      &ensp;{date}
-    </span>
-    &ensp;|&ensp;
-    <span>
-      <TimeIcon />
-      &ensp;{body.data.timeToRead} min read
-    </span>
-    &ensp;|&ensp;
-    <span>{categories.map(category =>
+  <Fragment>
+    <Meta inTitle={inTitle} >
+      <span>
+        <CreateIcon />
+        &nbsp;
+        <a href={`mailto:${author.email}`}>{author.name}</a>
+        <AuthorPhoto src={author.photo.file.url} />
+      </span>
+      <span>
+        <DateIcon />
+        &ensp;{date}
+      </span>
+      <span>
+        <TimeIcon />
+        &ensp;{body.data.timeToRead} min read
+      </span>
+    </Meta>
+    <Categories>{categories.map(category =>
       <Category key={category.slug}>{category.title}</Category>
-    )}</span>
-  </Meta>
+    )}</Categories>
+  </Fragment>
 )
 
 export default PostMeta
