6e65370ffc4d4b65071a9ee937f9bd197e88f61f
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-07-22 12:38:15 +0200

    style: PostTitle

--- src/components/PostTitle/styles.js
+++ src/components/PostTitle/styles.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components'
 import { Link } from 'gatsby'
 
+import mediaQuery from '../../utils/mediaQuery'
+
 export const Container = styled.header`
   color: ${props => props.theme.mainWhite};
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   margin: 10vh 0;
   a {
     color: inherit;
@@ -11,12 +15,20 @@ export const Container = styled.header`
 `
 
 export const Title = styled.h1`
-font-size: 1.6rem;
+  text-align: center;
+  margin: 1rem;
+  ${mediaQuery.minTablet} {
+    margin: 3rem;
+    font-size: 2rem;
+  }
 `
 
 export const BackLink = styled(Link)`
+  display: block;
+  margin-top: 2rem;
   width: max-content;
   padding: 0.2rem 0.6rem;
   border-radius: ${props => props.theme.largeBorderRadius};
+  border: ${props => props.theme.smallBorder} solid;
   background: rgba(0, 0, 0, 0.5);
 `
