530e8969f517f11c01ec7d63b12ff2b95f39f084
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-07-27 20:32:15 +0100

    feat: isolate LandingPage styles

---
+++ src/components/styles/Landing.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Landing = styled.article`
+  text-align: center;
+  color: ${props => props.theme.mainWhite};
+  * {
+    margin: 0 auto;
+  }
+`
+
+export default Landing
