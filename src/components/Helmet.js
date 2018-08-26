c43835b217e698feaab350e9076e0fb71493a507
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-08-26 15:49:04 +0200

    feat: construct helmet page title outside component

--- src/components/Helmet.js
+++ src/components/Helmet.js
@@ -3,8 +3,9 @@ import Helmet from 'react-helmet'
 
 const Head = ({ site, pageTitle, path, description }) => {
   const { title: siteTitle, url } = site.meta
+  const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
   return <Helmet>
-    <title>{`${pageTitle} | ${siteTitle}`}</title>
+    <title>{title}</title>
     <meta property="og:type" content="website" />
     <meta property="og:title" content={pageTitle} />
     <meta property="og:url" content={url + path} />
