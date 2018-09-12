158fd486f5c9267a736a914b84ce2f97f3cfa2a4
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-09-12 15:36:17 +0200

    feat: remove outsourced Nav from Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,62 +1,15 @@
 import React from 'react'
 
-import {
-  Content, Logo, SiteTitle,
-  Nav, NavLink, ToggleNav
-} from './styles'
-import logo from '../../assets/logo.svg'
+import { Container, Logo, SiteTitle } from './styles'
+import Nav from '../Nav'
 
-const events = ['mousedown', 'touchstart']
-
-class Header extends React.Component {
-  state = { hideNav: true }
-
-  toggleNav = () => {
-    this.setState({ hideNav: !this.state.hideNav })
-  }
-
-  handleClickOutside = (event) => {
-    if (this.node && !this.node.contains(event.target) && !this.state.hideNav) {
-      this.toggleNav()
-    }
-  }
-
-  componentDidMount() {
-    events.forEach(event =>
-      document.addEventListener(event, this.handleClickOutside)
-    )
-  }
-
-  componentWillUnmount() {
-    events.forEach(event =>
-      document.removeEventListener(event, this.handleClickOutside)
-    )
-  }
-  
-  render() {
-    const { meta, header } = this.props
-    return <Content>
-      <SiteTitle to="/" title={meta.title} rel="home">
-        <Logo src={logo} alt="Logo" />
-      </SiteTitle>
-      <Nav innerRef={node => this.node = node} hideNav={this.state.hideNav}>
-        <ToggleNav inside onClick={this.toggleNav}>&times;</ToggleNav>
-        {header.nav.map(item => (
-            <NavLink
-              onClick={this.toggleNav}
-              activeClassName='active'
-              key={item.url}
-              to={item.url}
-              title={item.title}
-              {...item.props}
-            >
-              {item.title}
-            </NavLink>
-        ))}
-      </Nav>
-      <ToggleNav onClick={this.toggleNav}>&#9776;</ToggleNav>
-    </Content>
-  }
-}
+const Header = ({ site }) => (
+  <Container>
+    <SiteTitle to="/" title={site.title} rel="home">
+      <Logo />
+    </SiteTitle>
+    <Nav />
+  </Container>
+)
 
 export default Header
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,83 +1,27 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import { Link } from 'gatsby'
 
-import mediaQuery from '../../utils/mediaQuery'
+import LogoComp from '../../assets/logo'
+import { navLinkStyle } from '../Nav/styles'
 
-const navLinkStyle = css`
-  color: inherit;
-  &:hover {
-    color: ${props => props.theme.mainOrange};
-    text-decoration: none;
-  }
-  &.${props => props.activeClassName} {
-    border-bottom: ${({ theme }) => theme.smallBorder + ` solid ` + theme.mainOrange};
-  }
-`
-
-export const Content = styled.header`
-  color: ${props => props.theme.mainWhite};
-  padding: 1rem 2rem;
+export const Container = styled.div`
+  grid-column: 1 / -1 !important;
+  grid-row: 1;
+  z-index: 2;
+  align-self: start;
+  background: rgba(0, 0, 0, 0.5);
   display: grid;
   grid-auto-flow: column;
-  justify-content: space-between;
+  grid-gap: 5vmin;
   align-items: center;
-  background: rgba(0, 0, 0, 0.3);
-  ${mediaQuery.minLaptop} {
-    justify-content: center;
-    padding: 1rem 2rem;
-    grid-gap: 3rem;
-  }
+  padding: 2vmin 3vmin;
+  justify-content: center;
 `
 
 export const SiteTitle = styled(Link)`
   ${navLinkStyle};
 `
 
-export const Logo = styled.img`
-  width: 6rem;
-  margin: 0;
-`
-
-export const Nav = styled.nav`
-  display: grid;
-  ${mediaQuery.minLaptop} {
-    grid-auto-flow: column;
-    grid-gap: 2rem;
-  }
-  ${mediaQuery.laptop} {
-    position: fixed;
-    right: 0;
-    border-left: ${({theme}) => theme.smallBorder + ' solid ' + theme.mainBlack};
-    z-index: 2;
-    background: ${props => props.theme.darkGray};
-    padding: 2rem;
-    grid-gap: 1rem;
-    height: 100vh;
-    min-width: 30vw;
-    grid-auto-rows: max-content;
-    transform: translate(${props => props.hideNav ? `100%` : `0`});
-    transition: transform 0.5s ease;
-  }
-`
-
-export const NavLink = styled(Link)`
-  ${navLinkStyle};
-`
-
-export const ToggleNav = styled.button`
-  color: inherit;
-  transform: scale(2);
-  background: transparent;
-  border: none;
-  padding: 0;
-  cursor: pointer;
-  outline: none;
-  ${mediaQuery.minLaptop} {
-    display: none;
-  }
-  ${props => props.inside && `
-    position: absolute;
-    top: 0.7rem;
-    right: 1.3rem;
-  `}
+export const Logo = styled(LogoComp)`
+  width: 5em;
 `
