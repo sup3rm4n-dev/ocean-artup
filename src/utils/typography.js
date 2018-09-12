d34ecc291050bcd0fe709636050fd4163bdc5e7d
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-09-12 16:40:23 +0200

    feat: define font families, min and max sizes in utils/typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,12 +1,10 @@
-import Typography from 'typography'
-import noriega from 'typography-theme-noriega'
-
-noriega.overrideThemeStyles = () => ({
-  'a': {
-    textDecoration: 'none'
-  }
-})
-
-const typography = new Typography(noriega)
+const typography = {
+  fonts: `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif`,
+  // font sizes and line heigths in em units
+  minFontSize: 1,
+  maxFontSize: 1.3,
+  minLineHeight: 1.5,
+  maxLineHeight: 2,
+}
 
 export default typography
