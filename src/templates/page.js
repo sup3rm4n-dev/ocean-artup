e30897f6e367268a8b09c5be6600b8a2f0b7a243
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-09-12 16:42:54 +0200

    feat: remove Helmet and siteMetaQuery from page template

--- src/templates/page.js
+++ src/templates/page.js
@@ -2,18 +2,14 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
-import Helmet from '../components/Helmet'
 import PageTitle from '../components/PageTitle'
 import PageBody from '../components/PageBody'
 
-const PageTemplate = ({ data, location }) => {
-  const { page, site } = data
+const PageTemplate = ({ data: { page }, location }) => {
   const { title, body } = page
   const { excerpt, htmlAst } = body && body.data
-  const path = location.pathname
   return (
-    <Layout>
-      <Helmet pageTitle={title} site={site} path={path} description={excerpt} />
+    <Layout pageTitle={title} path={location.pathname} description={excerpt}>
       <PageTitle title={title} />
       {htmlAst && <PageBody htmlAst={htmlAst} />}
     </Layout>
@@ -22,18 +18,9 @@ const PageTemplate = ({ data, location }) => {
 
 export default PageTemplate
 
-export const pageQuery = graphql`
-  fragment siteMetaQuery on RootQueryType {
-    site {
-      meta: siteMetadata {
-        title
-        url: siteUrl
-      }
-    }
-  }
+export const query = graphql`
   query($slug: String!) {
-    ...siteMetaQuery
-    page: contentfulPage(slug: {eq: $slug}) {
+    page: contentfulPage(slug: { eq: $slug }) {
       title
       slug
       body {
