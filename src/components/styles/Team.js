286a28a0509dc8aa94f2587791536dc7c92f39f5
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-09-12 23:33:37 +0200

    style: simplify Team

--- src/components/styles/Team.js
+++ src/components/styles/Team.js
@@ -1,13 +1,9 @@
 import styled from 'styled-components'
 
-import mediaQuery from '../../utils/mediaQuery'
-
 const Team = styled.div`
   display: grid;
   grid-gap: calc(0.5em + 4vmin);
-  grid-template-columns: repeat(auto-fit, minmax(auto, 10rem));
-  justify-content: center;
-  max-width: ${props => props.theme.maxWidth};
+  grid-template-columns: repeat(auto-fit, minmax(8em, 1fr));
 
   p:first-child,
   h1,
@@ -17,28 +13,11 @@ const Team = styled.div`
   h5,
   h6 {
     grid-column: 1/-1;
-    max-width: 100%;
     text-align: center;
-  }
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    white-space: nowrap;
-    font-size: 2rem;
-    ${mediaQuery.minPhone} {
-      font-size: 2.5rem;
-    }
-    ${mediaQuery.minPhablet} {
-      font-size: 3rem;
-    }
   }
   .gatsby-resp-image-wrapper {
     border-radius: 50%;
     overflow: hidden;
-    width: 100%;
   }
 
   p {
@@ -47,17 +26,14 @@ const Team = styled.div`
     img {
       margin: 0;
       border-radius: 50%;
-      width: 100%;
-      max-width: 10rem;
     }
     em {
       display: block;
-      font-style: normal;
-      font-weight: bold;
-    }
-    strong {
-      display: block;
       font-weight: lighter;
+      font-style: normal;
+    }
+    em + em {
+      font-size: 0.9em;
     }
   }
 `
