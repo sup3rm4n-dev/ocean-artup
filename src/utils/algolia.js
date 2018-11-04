7bf7c1a65fd289f9f34eb885cea28d8fa53e1bbb
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-11-04 17:25:55 +0000

    feat: add src/utils/algolia

---
+++ src/utils/algolia.js
@@ -0,0 +1,80 @@
+const pageQuery = `{
+  pages: allContentfulPage {
+    edges {
+      node {
+        objectID: id
+        slug
+        title
+        body {
+          data: childMarkdownRemark {
+            excerpt(pruneLength: 5000)
+            headings {
+              value
+              depth
+            }
+          }
+        }
+      }
+    }
+  }
+}`
+
+const postQuery = `{
+  posts: allContentfulBlogPost {
+    edges {
+      node {
+        objectID: id
+        slug
+        title
+        date(formatString: "MMMM DD, YYYY")
+        author {
+          name
+          email
+        }
+        categories {
+          title
+          slug
+        }
+        body {
+          data: childMarkdownRemark {
+            excerpt(pruneLength: 5000)
+            headings {
+              value
+              depth
+            }
+          }
+        }
+      }
+    }
+  }
+}`
+
+const queries = [
+  {
+    query: pageQuery,
+    transformer: ({ data }) =>
+      data.pages.edges.map(
+        ({ node: { body, ...rest } }) =>
+          [`Error 404`].includes(rest.title)
+            ? {}
+            : {
+                ...body.data,
+                ...rest,
+              }
+      ),
+    indexName: `Pages`,
+    settings: { attributesToSnippet: [`excerpt:20`] },
+  },
+  {
+    query: postQuery,
+    transformer: ({ data }) =>
+      data.posts.edges.map(({ node: { body, ...rest } }) => ({
+        ...body.data,
+        ...rest,
+      })),
+    indexName: `Posts`,
+    settings: { attributesToSnippet: [`excerpt:20`] },
+  },
+]
+
+module.exports = queries
