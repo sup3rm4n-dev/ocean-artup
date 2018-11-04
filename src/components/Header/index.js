eefd6a9a8900b66293039d93b8581713680329b1
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-11-04 17:27:05 +0000

    feat: add Search to Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 import { Container, SiteTitle } from './styles'
 import Nav from '../Nav'
+import Search from '../Search'
 import Logo from '../../assets/logo'
+
+const searchIndices = [
+  { name: `Pages`, title: `Pages`, hitComp: `PageHit` },
+  { name: `Posts`, title: `Blog Posts`, hitComp: `PostHit` },
+]
 
 const Header = ({ site }) => (
   <Container>
@@ -10,6 +16,7 @@ const Header = ({ site }) => (
       <Logo />
     </SiteTitle>
     <Nav />
+    <Search collapse indices={searchIndices} />
   </Container>
 )
 
