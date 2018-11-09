74620b5ee171feaff01ef7bb0611430d07fa5e11
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-11-09 23:01:07 +0000

    feat: add featuredImage to PostExcerpt

--- src/components/PostExcerpt/index.js
+++ src/components/PostExcerpt/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import Img from 'gatsby-image'
 
-import { Article, Title } from './styles'
+import { Article, Title, Excerpt } from './styles'
 import PostMeta from '../PostMeta'
 
-const PostExcerpt = ({ post, post: { title, slug, body } }) => (
+const PostExcerpt = ({
+  post,
+  post: { title, slug, body, featuredImage: featImg },
+}) => (
   <Article>
-    <Title to={'/blog/' + slug}>{title}</Title>
-    <PostMeta {...post} />
-    <p dangerouslySetInnerHTML={{ __html: body.data.excerpt }} />
+    {featImg && <Img fluid={featImg.fluid} alt={featImg.title} />}
+    <div>
+      <Title to={'/blog/' + slug}>{title}</Title>
+      <PostMeta {...post} />
+      <Excerpt dangerouslySetInnerHTML={{ __html: body.data.excerpt }} />
+    </div>
   </Article>
 )
 
