22c04efb95924bb24c98e87ce22c1a1337e9eec2
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-11-09 23:01:40 +0000

    style: PostExcerpt featuredImage

--- src/components/PostExcerpt/styles.js
+++ src/components/PostExcerpt/styles.js
@@ -3,17 +3,23 @@ import { Link } from 'gatsby'
 
 export const Article = styled.article`
   background: ${props => props.theme.veryLightGray};
-  padding: 0.7em 1em;
   border-radius: ${props => props.theme.mediumBorderRadius};
-  p {
-    margin-bottom: 0;
+  overflow: hidden;
+  .gatsby-image-wrapper {
+    height: 25vh;
   }
-  & + & {
-    margin-top: 3em;
+  > div {
+    padding: 0.7em 1em;
+    display: grid;
+    grid-gap: 0.5em;
   }
 `
 
 export const Title = styled(Link)`
-  font-size: 1.6em;
+  font-size: 1.3em;
   font-weight: bold;
+`
+
+export const Excerpt = styled.p`
+  margin: 0;
 `
