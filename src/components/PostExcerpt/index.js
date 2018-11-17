49944e6a135fabe5b1841188af25265963f58e15
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-11-17 09:25:45 +0000

    refactor: PostExcerpt

--- src/components/PostExcerpt/index.js
+++ src/components/PostExcerpt/index.js
@@ -2,17 +2,14 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Img from 'gatsby-image'
 
-import { Article, Title, Excerpt } from './styles'
+import { Post, Title, Excerpt } from './styles'
 import PostMeta from '../PostMeta'
 
-const PostExcerpt = ({
-  post,
-  post: { title, slug, body, featuredImage: featImg },
-}) => (
-  <Article>
-    {featImg && (
+const PostExcerpt = ({ post, post: { title, slug, body, featuredImage } }) => (
+  <Post>
+    {featuredImage && (
       <Link to={'/blog/' + slug}>
-        <Img fluid={featImg.fluid} alt={featImg.title} />
+        <Img fluid={featuredImage.fluid} alt={featuredImage.title} />
       </Link>
     )}
     <div>
@@ -22,7 +19,7 @@ const PostExcerpt = ({
       <PostMeta {...post} />
       <Excerpt dangerouslySetInnerHTML={{ __html: body.data.excerpt }} />
     </div>
-  </Article>
+  </Post>
 )
 
 export default PostExcerpt
--- src/components/PostExcerpt/styles.js
+++ src/components/PostExcerpt/styles.js
@@ -1,15 +1,12 @@
 import styled from 'styled-components'
 
-export const Article = styled.article`
+export const Post = styled.article`
   height: 100%;
   display: grid;
   background: ${props => props.theme.veryLightGray};
   border-radius: ${props => props.theme.mediumBorderRadius};
   border: 1px solid ${props => props.theme.lightGray};
   overflow: hidden;
-  .gatsby-image-wrapper {
-    height: 25vh;
-  }
   > div {
     padding: 0.7em 1em;
     display: grid;
