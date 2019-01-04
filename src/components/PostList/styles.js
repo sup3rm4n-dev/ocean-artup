c48f152146a657049edf3371cdda619cd6c2ac17
JanoshRiebesell <janosh.riebesell@gmail.com>
2019-01-04 20:02:46 +0000

    style: PostList remove Masonry, simple grid instead

--- src/components/PostList/styles.js
+++ src/components/PostList/styles.js
@@ -5,4 +5,7 @@ export const Container = styled.div`
   line-height: 1.4em;
   grid-column: 2/-2;
   margin: calc(2em + 2vh) 0;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(16em, 1fr));
+  grid-gap: 1em;
 `
