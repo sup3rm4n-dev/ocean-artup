93ff463de4f502335528b1a5a18d3f7a48a456af
janosh <janosh.riebesell@gmail.com>
2019-04-14 19:19:02 +0100

    refactor: rewrite Nav as functional comp using react-spring on mobile

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,8 +1,19 @@
-import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
-import PropTypes from 'prop-types'
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import PropTypes from "prop-types"
 
-import Nav from './comp'
+import { useMediaQuery } from "../../utils/mediaQuery"
+
+import MobileNav from "./Mobile"
+import DesktopNav from "./Desktop"
+export { navLinkStyle, NavLink } from "./styles"
+
+const Nav = props =>
+  useMediaQuery(`maxPhablet`) ? (
+    <MobileNav {...props} />
+  ) : (
+    <DesktopNav {...props} />
+  )
 
 const query = graphql`
   {
@@ -20,15 +31,15 @@ const query = graphql`
 export default props => (
   <StaticQuery
     query={query}
-    render={data => <Nav {...data.nav.data} {...props} />}
+    render={data => <Nav {...data.nav.data} {...props} role="navigation" />}
   />
 )
 
 Nav.propTypes = {
   nav: PropTypes.arrayOf(
     PropTypes.shape({
-      url: PropTypes.string.isRequired,
       title: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
     })
   ).isRequired,
 }
--- src/components/Nav/styles.js
+++ src/components/Nav/styles.js
@@ -1,101 +1,20 @@
 import styled, { css } from 'styled-components'
 import { Link } from 'gatsby'
 
-import mediaQuery from '../../utils/mediaQuery'
-
 export const navLinkStyle = css`
-  color: ${props => props.theme.white};
+  color: white;
   transition: ${props => props.theme.shortTrans};
+  &.active {
+    color: ${props => props.theme.orange};
+  }
   :hover {
-    color: ${props => props.theme.orange};
-  }
-  &.${props => props.activeClassName} {
-    color: ${props => props.theme.orange};
+    color: ${props => props.theme.lightBlue};
   }
 `
 
-export const NavContainer = styled.nav`
-  display: grid;
-  grid-gap: calc(0.4em + 2vw);
-  grid-auto-columns: max-content;
-  ${mediaQuery.maxPhablet} {
-    overflow-x: scroll;
-    position: fixed;
-    right: 100%;
-    z-index: 2;
-    background: ${props => props.theme.darkGray};
-    padding: 5vh;
-    grid-gap: 1em;
-    height: 100vh;
-    min-width: 20vw;
-    grid-auto-rows: max-content;
-    transform: translate(${props => (props.showNav ? `99%` : `0`)});
-    transition: ${props => props.theme.mediumTrans};
-  }
-  ${mediaQuery.minPhablet} {
-    grid-auto-flow: column;
-  }
-`
-
-export const NavEntry = styled.div`
-  position: relative;
-`
-
-const showSubNav = css`
-  display: grid;
-  visibility: visible;
-  opacity: 1;
-`
-
-export const SubNav = styled.div`
-  display: grid;
-  width: max-content;
-  border-radius: ${props => props.theme.smallBorderRadius};
-  grid-gap: 0.5em;
-  background: ${props => props.theme.gray};
-  opacity: 0;
-  position: absolute;
-  transition: opacity 0.25s;
-  padding: 0.7em 1em;
-  ${mediaQuery.maxPhablet} {
-    ${props => props.showNav && showSubNav + `position: static;`};
-  }
-  ${mediaQuery.minPhablet} {
-    ${NavEntry}:hover & {
-      left: 0;
-      z-index: 2;
-      ${showSubNav}
-    }
-  }
-`
-
-export const NavLink = styled(Link)`
-  ${navLinkStyle};
-  ${SubNav} & {
-    color: ${props => props.theme.white};
-    :hover {
-      color: ${props => props.theme.darkBlue};
-    }
-  }
-`
-
-const inNavToggle = css`
-  position: absolute;
-  top: 0.3em;
-  right: 0.5em;
-`
-
-const inHeaderToggle = css`
-  grid-area: 1 / 1 / 1 / 1;
-`
-
-export const Toggle = styled.span`
-  font-size: 1.8em;
-  cursor: pointer;
-  width: max-content;
-  ${mediaQuery.minPhablet} {
-    display: none;
-  }
-  ${props => (props.inside ? inNavToggle : inHeaderToggle)};
+export const NavLink = styled(Link).attrs({
+  activeClassName: `active`,
+  partiallyActive: true,
+})`
   ${navLinkStyle};
 `
