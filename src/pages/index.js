368205eb76b7b59673e38c75da6f2ab0a71cf45a
janosh <janosh.riebesell@gmail.com>
2019-04-14 19:33:17 +0100

    refactor: IndexPage change PageTitle

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import { graphql } from 'gatsby'
+import React from "react"
+import { graphql } from "gatsby"
 
-import Global from '../components/Global'
-import PageTitle from '../components/PageTitle'
-import Scroll from '../components/Scroll'
-import LandingBody from '../components/styles/LandingBody'
+import Global from "../components/Global"
+import PageTitle from "../components/PageTitle"
+import Scroll from "../components/Scroll"
+import { LandingBody } from "../components/styles"
 
 const IndexPage = ({ data: { page }, location }) => {
   const { title, subtitle, body } = page
-  const { excerpt, html } = body && body.data
+  const { excerpt, html } = body && body.remark
   return (
     <Global pageTitle={title} path={location.pathname} description={excerpt}>
-      <PageTitle title={title} subtitle={subtitle} />
+      <PageTitle height="100vh">
+        <h1>{title}</h1>
+        <p dangerouslySetInnerHTML={{ __html: subtitle.remark.html }} />
+      </PageTitle>
       <Scroll direction="down" to={1} align="center" position="absolute" />
       {html && <LandingBody dangerouslySetInnerHTML={{ __html: html }} />}
     </Global>
@@ -23,14 +26,7 @@ export default IndexPage
 export const query = graphql`
   {
     page: contentfulPage(slug: { eq: "/" }) {
-      title
-      subtitle
-      body {
-        data: childMarkdownRemark {
-          excerpt
-          html
-        }
-      }
+      ...pageFields
     }
   }
 `
