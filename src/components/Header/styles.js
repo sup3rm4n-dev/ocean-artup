4cc2cdb880c4f2c1bc8ffc3766622dfe2c2a65d0
janosh <janosh.riebesell@gmail.com>
2019-04-14 19:18:00 +0100

    refactor: Header style

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,28 +1,28 @@
-import styled from 'styled-components'
-import { Link } from 'gatsby'
+import styled from "styled-components"
+import { Link } from "gatsby"
 
-import { navLinkStyle } from '../Nav/styles'
-import mediaQuery from '../../utils/mediaQuery'
+import { navLinkStyle } from "../Nav/styles"
+import mediaQuery from "../../utils/mediaQuery"
 
 export const HeaderContainer = styled.header`
-  grid-column: 1 / -1;
-  grid-row: 1;
+  position: absolute;
+  width: fill-available;
   z-index: 3;
-  align-self: start;
   background: rgba(0, 0, 0, 0.6);
   display: grid;
   grid-auto-flow: column;
   grid-gap: 3vw;
   align-items: center;
   padding: 1vh calc(0.75em + 1vw);
-  grid-template-areas: 'nav title search';
+  grid-template-areas: "nav title search";
   grid-template-columns: auto 1fr auto;
-  ${mediaQuery.maxTablet} {
-    grid-template-areas: 'title nav search';
+  ${mediaQuery.minTablet} {
+    grid-template-areas: "title nav search";
   }
 `
 
 export const SiteTitle = styled(Link)`
+  grid-area: title;
   width: 5em;
   display: inline-grid;
   ${navLinkStyle};
