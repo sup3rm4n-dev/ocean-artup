9ec924c61ef46e958d45352ebccdbee9b941b900
janosh <janosh.riebesell@gmail.com>
2019-08-05 12:52:40 +0100

    chore: eslint add plugin react-hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,5 @@
-/*
-#!/bin/sh
-#
-# Pre-commit hook called by "git commit" with no arguments.
-# Checks staged .js(x) files for eslint errors.
-# Exits with non-zero status after issuing an appropriate message if
-# it wants to stop the commit.
-# Place in {projectRoot}/.git/hooks/pre-commit
-
-git diff --diff-filter=d --cached --name-only | grep "\.js.\?$" | xargs ./node_modules/.bin/eslint
-*/
+// These settings are used by a git pre-commit hook in src/utils/gitHooks
+// ensuring eslint compliance before allowing a commit to go through.
 
 module.exports = {
   env: {
@@ -16,27 +7,30 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  parser: "babel-eslint",
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react"],
+  plugins: ['react', 'react-hooks'],
   rules: {
-    indent: ["error", 2, { SwitchCase: 1 }],
-    quotes: ["error", "backtick", { avoidEscape: true }],
-    semi: ["error", "never"],
-    "linebreak-style": ["error", "unix"],
-    "react/prop-types": "off",
-    "react/display-name": "off",
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'backtick', { avoidEscape: true }],
+    semi: ['error', 'never'],
+    'linebreak-style': ['error', 'unix'],
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
 }
