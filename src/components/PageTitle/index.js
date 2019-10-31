12657fa6db89cdcfd53e2f6ec2be7c3e6f1afd5b
janosh <janosh.riebesell@gmail.com>
2019-10-31 17:31:46 +0000

    refactor: PageTitle to useStaticQuery

--- src/components/PageTitle/index.js
+++ src/components/PageTitle/index.js
@@ -1,26 +1,20 @@
-import React from "react"
-import { StaticQuery, graphql } from "gatsby"
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 
-import { TitleDiv, Title, Img } from "./styles"
+import { TitleDiv, Title, Img } from './styles'
 
-const PageTitle = ({ children, img, defaultImg, height, backdrop }) => (
-  <TitleDiv height={height}>
-    <Img fluid={(img || defaultImg).fluid} alt={(img || defaultImg).title} />
-    <Title backdrop={backdrop}>{children}</Title>
-  </TitleDiv>
-)
-
-const query = graphql`
-  {
-    defaultImg: contentfulAsset(title: { eq: "Background" }) {
-      ...coverFields
+export default function PageTitle({ children, height, backdrop }) {
+  const { img, defaultImg } = useStaticQuery(graphql`
+    {
+      defaultImg: contentfulAsset(title: { eq: "Background" }) {
+        ...coverFields
+      }
     }
-  }
-`
-
-export default props => (
-  <StaticQuery
-    query={query}
-    render={data => <PageTitle {...data} {...props} />}
-  />
-)
+  `)
+  return (
+    <TitleDiv height={height}>
+      <Img fluid={(img || defaultImg).fluid} alt={(img || defaultImg).title} />
+      <Title backdrop={backdrop}>{children}</Title>
+    </TitleDiv>
+  )
+}
